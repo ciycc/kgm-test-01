3a5cf53f189230c4be02c4d0f6076c3fc97bd41b
Arjun <hangukyong2@gmail.com>
2024-06-03 02:17:01 +0900

    가이드 컬러 가이드 추가

--- src/publishing/components/CompColor.vue
+++ src/publishing/components/CompColor.vue
@@ -1,18 +1,49 @@
 <template>
   <div>
-    <ul>
-      <li>
-        <div>색상</div>
-        <div>생상번호</div>
+    <ul class="items-color-list">
+      <li v-for="item in items">
+        <div class="item-color-box" :style="{'background': item.color, 'color': item.fc}">{{ item.color }}</div>
+        <div class="item-color-name"  :style="{'background': item.color}"><button class="btn-color-name cursor-zoom" :style="{'color': item.fc}" @click="colorNameCopyHandler">{{ item.name }}</button></div>
       </li>
     </ul>
   </div>
 </template>
 
 <script setup>
+  const { items } = defineProps({
+    items: {
+      type: Array,
+      default: []
+    },
+  });
+  const colorNameCopyHandler = (e) => {
+    const self = e.target;
+    let tempInput = document.createElement("input")
+    tempInput.value = `var(${self.textContent})`;
 
+    document.body.appendChild(tempInput);
+    tempInput.select();
+    document.execCommand("copy");
+    document.body.removeChild(tempInput);
+  }
 </script>
 
 <style lang="scss" scoped>
-
+.items-color-list {
+  .item-color-box,
+  .item-color-name {
+    font-size: rem(13);
+    width: rem(200);
+    padding: rem(10) 0 0;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-weight: 400;
+  }
+  .item-color-name {
+    line-height: 1;
+    padding: 0 rem(10) rem(10);
+    font-size: rem(15);
+  }
+}
 </style>
--- src/publishing/guide/doc/color/md1.md
+++ src/publishing/guide/doc/color/md1.md
@@ -2,28 +2,67 @@
 
 # Color
 
-## 메인컬러
-
 </div>
+
 <script setup>
 import CompColor from "&/components/CompColor.vue";
+const colorItems = {
+  primary: [
+    {
+      name:"--gray-100",
+      color:"#333",
+      fc:"#fff",
+    },
+    {
+      name:"--gray-200",
+      color:"#555",
+      fc:"#fff",
+    },
+    {
+      name:"--gray-300",
+      color:"#666",
+      fc:"#fff",
+    },
+    {
+      name:"--gray-400",
+      color:"#999",
+      fc:"#111",
+    },
+    {
+      name:"--gray-500",
+      color:"#ccc",
+      fc:"#111",
+    },
+  ],
+  secondary: [],
+  point:[],
+  error:[],
+}
 </script>
 
-<CompColor />
-<div class="md-section">
-
-## 서브컬러
-
-</div>
-
-<div class="md-section">
-
-## 강조컬러
-
-</div>
-
-<div class="md-section">
-
-## 폰트컬러
-
+<div class="color-guide-list">
+  <div class="color-item">
+    <div class="md-section">
+      <h2>Primarry Colors</h2>
+    </div>
+    <CompColor :items="colorItems.primary" />
+  </div>
+  
+  <div class="color-item">
+    <div class="md-section">
+      <h2>Secondary Colors</h2>
+    </div>
+  </div>
+  
+  <div class="color-item">
+    <div class="md-section">
+      <h2>Point Colors</h2>
+    </div>
+  </div>
+  
+  <div class="color-item">
+    <div class="md-section">
+      <h2>Error Colors</h2>
+    </div>
+  </div>
 </div>
